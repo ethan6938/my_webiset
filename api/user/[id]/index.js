031aa62ab194723e4ba9700a703e3c976dc0a526
ethan6938 <en.hurwitz@gmail.com>
2025-04-23 17:39:04 +0200

    Update

--- api/user/[id]/index.js
+++ api/user/[id]/index.js
@@ -9,18 +9,16 @@ export const config = {
 };
 
 export default async function handler(req, res) {
+  const method = req.body?._method || req.method;
+
+  if (method !== 'PUT') {
+    return res.status(405).send('Method Not Allowed');
+  }
+
   try {
-    const method = req.body?._method || req.method;
-    console.log('➡️ METHOD:', method);
-    console.log('📦 BODY:', req.body);
-
-    if (method !== 'PUT') {
-      return res.status(405).send('Method Not Allowed');
-    }
-
     const { name = 'Unknown', bio = 'No bio provided' } = req.body;
 
-    return res.status(200).send(`
+    res.status(200).send(`
       <div class="card fade-in" style="width: 22rem;" hx-target="this" hx-swap="outerHTML">
         <div class="card-body text-center">
           <h5 class="card-title">${escapeHtml(name)}</h5>
@@ -32,7 +30,7 @@ export default async function handler(req, res) {
       </div>
     `);
   } catch (err) {
-    console.error('💥 ERROR:', err);
-    return res.status(500).send('Internal Server Error');
+    console.error('💥 Error:', err);
+    res.status(500).send('Internal Server Error');
   }
 }
