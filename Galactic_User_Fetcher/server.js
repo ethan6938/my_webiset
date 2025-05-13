7a59f34c775dad1cf8e3bbb809bead8a69a9d67c
ethan6938 <en.hurwitz@gmail.com>
2025-05-13 13:03:53 +0200

    Update

--- Galactic_User_Fetcher/server.js
+++ Galactic_User_Fetcher/server.js
@@ -22,17 +22,17 @@ app.get('/users', async (req, res) => {
     <div class="card-neon text-start">
       <h2 class="text-center">👨‍🚀 User Profiles</h2>
       <ul class="list-group list-group-flush">
-        $}users.map(user => {
-          const avatarUrl = \`https://api.dicebear.com/7.x/thumbs/svg?seed=\${encodeURIComponent(user.username)}\`;
-          return \`
+        ${users.map(user => {
+          const avatarUrl = `https://api.dicebear.com/7.x/thumbs/svg?seed=${encodeURIComponent(user.username)}`;
+          return `
             <li class="list-group-item bg-transparent text-light d-flex align-items-center border-light">
-              <img src="\${avatarUrl}" class="user-avatar" alt="Avatar of \${user.name}" />
+              <img src="${avatarUrl}" class="user-avatar" alt="Avatar of ${user.name}" />
               <div>
-                <strong>\${user.name}</strong><br>
-                <small>📧 \${user.email}</small>
+                <strong>${user.name}</strong><br>
+                <small>📧 ${user.email}</small>
               </div>
             </li>
-          \`;
+          `;
         }).join('')}
       </ul>
     </div>
