caded4dac52f3c58531f54c24d27bb0eff20189d
ethan6938 <en.hurwitz@gmail.com>
2025-05-26 16:49:29 +0200

    Update

--- api/user/1.js
+++ api/user/1.js
@@ -1,6 +1,6 @@
-const escapeHtml = require('escape-html');
+import escapeHtml from 'escape-html';
 
-module.exports = function handler(req, res) {
+export default function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).send('Method Not Allowed');
   }
