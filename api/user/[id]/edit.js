6721281793699e1bd407ab65468baf874128c531
ethan6938 <en.hurwitz@gmail.com>
2025-05-27 13:57:24 +0200

    Update

--- api/user/[id]/edit.js
+++ api/user/[id]/edit.js
@@ -1,13 +1,11 @@
 export default function handler(req, res) {
-  const id = req.query.id;
+  const { id } = req.query;
 
-  res.status(200).send(`
-    <form class="fade-in" method="POST" 
-          hx-post="/api/user/${id}" 
-          hx-target="this" 
+  const html = `
+    <form class="fade-in"
+          hx-put="/api/user/${id}"
+          hx-target="this"
           hx-swap="outerHTML">
-      <input type="hidden" name="_method" value="PUT">
-
       <div class="mb-3">
         <label for="name" class="form-label text-info">Name</label>
         <input type="text" class="form-control" id="name" name="name" value="Greg Lim">
@@ -17,7 +15,10 @@ export default function handler(req, res) {
         <textarea class="form-control" id="bio" name="bio" rows="3">Follower of Christ | Author of Best-selling Amazon Tech Books and Creator of Coding Courses</textarea>
       </div>
       <button type="submit" class="btn btn-primary w-100 mb-2">Save Changes</button>
-      <button class="btn btn-primary w-100" hx-get="/index.html">Cancel</button>
+      <button type="button" class="btn btn-secondary w-100" hx-get="/">Cancel</button>
     </form>
-  `);
+  `;
+
+  res.setHeader("Content-Type", "text/html");
+  return res.status(200).send(html);
 }
